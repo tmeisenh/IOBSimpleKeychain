8edf77da6d123e8ae159288ec70737f5e2bb58c1
Travis 'All-Father' Meisenheimer <tmeisenh@gmail.com>
2015-07-08 20:21:37 -0500

    Added default ErrorDescription to protect against nil errorMessages being added to dictionary

--- IOBSimpleKeychain/IOBErrorHelper.m
+++ IOBSimpleKeychain/IOBErrorHelper.m
@@ -19,9 +19,11 @@
         return NO;
     }
     
+    NSString *errorUserInfoDescription = errorMessage ?: @"Consider adding an error message to aid debugging.";
+
     *error = [NSError errorWithDomain:@"com.indexoutofbounds.iobsimplekeychain"
                                  code:errorCode
-                             userInfo:@{NSLocalizedDescriptionKey : errorMessage}];
+                             userInfo:@{NSLocalizedDescriptionKey : errorUserInfoDescription}];
     return YES;
 }
 
